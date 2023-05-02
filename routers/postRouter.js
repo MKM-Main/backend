c178f7eb99aface178c917cfa04388f5079b40ab
Kevin <kevi4777@stud.kea.dk>
2023-05-02 20:17:20 +0200

    ddl fix and postRouter reversed

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -65,31 +65,28 @@ router.post('/api/posts/:reference', authenticateToken, async (req, res) => {
 });
 
 
+
   router.patch("/api/posts/comments/:reference/:search", authenticateToken, async (req, res) => {
-  const userLoggedIn = req.user.artistName;
-  const comment = req.body;
-  comment.commentAuthor = userLoggedIn;
+    const userLoggedIn = req.user.artistName;
+    const comment = req.body;
+    comment.commentAuthor = userLoggedIn;
     comment._id = new ObjectId();
     comment.rating = 0;
-
     comment.timeCreated = new Date().toLocaleString("en-GB"); 
-  
-    if (req.params.reference === "wallposts") {
+    
+    if(req.params.reference === "wallposts"){
       const id = req.params.search
       const result = await db.posts.updateOne(
-        { _id: new ObjectId(id) },
-        { $push: { comments: comment } }
-      );
-      res.status(200).send({ message: comment });
-    } else {
+              { _id: new ObjectId(id) },
+              { $push: { comments: comment } }
+            );
+            res.status(200).send({ message: comment });
+    }else{
       const postTitle = req.params.search
-      const updateCommentArray = await db.posts.updateOne(
-        { postTitle: postTitle },
-        { $push: { comments: { ...comment, _id: new ObjectId() } } }
-      )
+      const updateCommentArray = await db.posts.updateOne({postTitle: postTitle}, {$push: {comments: comment}})
       res.status(200).send(comment)
     }
-  });
+});
 
 
 router.delete("/api/posts/comments/:postid/:commentid", async (req, res) => {
