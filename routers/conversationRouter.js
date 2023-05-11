2080bbd9aea05c705f2f6febdcd84a405e7588eb
MichaelDyvad <michaeldyvad@gmail.com>
2023-05-11 12:01:25 +0200

    conversation logic added

--- routers/conversationRouter.js
+++ routers/conversationRouter.js
@@ -24,10 +24,19 @@ router.get("/api/conversations/:conversationid", authenticateToken, async (req, 
 
 //Find all conversations on a logged in user
 router.get("/api/conversations", authenticateToken, async (req, res) => {
-    const user = req.user.artistName;
-    const conversations = await db.conversations.find({ participants: { $in: [user] }}).project({participants: { $elemMatch: { $ne: user }}}).toArray();
-    res.send(conversations)
+  const user = req.user.artistName;
+  const conversations = await db.conversations.find({ participants: { $in: [user] } }).project({ participants: { $elemMatch: { $ne: user } }, timeStamp: 1 }).sort({ timeStamp: -1 }).toArray();
+  
+  const arrayParticipants = conversations.map(conversation => conversation.participants).flat();
+  const findUsers = await db.users.find({ artistName: { $in: arrayParticipants } }).project({ profilePictureKey: 1, artistName: 1}).toArray();
+
+  const mergedConversations = conversations.map((conversation) => {
+    const user = findUsers.find(u => u.artistName === conversation.participants[0]);
+    return { ...conversation, profilePictureKey: user.profilePictureKey };
   });
+
+  res.status(200).send(mergedConversations);
+});
 
 //Creates a conversation with another user
 router.post("/api/conversations/:receiver", authenticateToken, async (req, res) => {
@@ -37,10 +46,16 @@ router.post("/api/conversations/:receiver", authenticateToken, async (req, res) 
     const conversation = req.body;
     conversation.participants = [sender, receiver];
     conversation.messages = [];
+    conversation.timeStamp = new Date().toLocaleString("en-GB");
 
-    const insertConversation = await db.conversations.insertOne(conversation);
-
-    res.status(200).send(insertConversation);
+    const existingConversation = await db.conversations.findOne({participants: {$all: [sender, receiver]}});
+    if(existingConversation){
+      res.send({message: "Conversation already exist"})
+    }else{
+      const insertConversation = await db.conversations.insertOne(conversation);
+      const getImg = await db.users.findOne({artistName: receiver})
+      res.status(200).send([{participants: [receiver], _id: insertConversation.insertedId, profilePictureKey: getImg.profilePictureKey, timeStamp: conversation.timeStamp}]);
+    }
 })
 
 //Patch message to a conversation
@@ -48,19 +63,31 @@ router.patch("/api/conversations/messages/:conversationid", authenticateToken, a
     const user = req.user;
     const conversationId = req.params.conversationid;
     const sender = user.artistName
-    // const receiver = req.params.receiver;
-
+    const timeStamp = new Date().toLocaleString("en-GB");
     const message = req.body;
 
-    message.sender = sender;
-    // message.receiver = receiver;
-    message._id = new ObjectId();
-    message.timeStamp = new Date().toLocaleString("en-GB");
+    // const conversation = await db.conversations.findOne({ _id: new ObjectId(conversationId) });
+    // const receiver = conversation.participants.filter(p => sender !== p);
+    const findImg = await db.users.findOne({artistName: sender})
 
+    message.profilePictureKey = findImg.profilePictureKey
+
+    message.sender = sender;
+    message._id = new ObjectId();
+    message.timeStamp = timeStamp
+    const updateConversation = await db.conversations.updateOne(
+      { _id: new ObjectId(conversationId) },
+      { $set: { timeStamp: timeStamp } }
+    );
     const insertMessage = await db.conversations.updateOne({ _id: new ObjectId(conversationId) }, { $push: { messages: message } });
-    res.status(200).send({ message: insertMessage });
+    res.status(200).send({ message: message });
 })
 
+router.delete("/api/conversations/:conversationid", async (req, res) => {
+    const conversationId = new ObjectId(req.params.conversationid);
+    const deleteConversation = await db.conversations.deleteOne({_id : conversationId})
+    res.status(200).send(deleteConversation)
+})
 
 
 export default router
