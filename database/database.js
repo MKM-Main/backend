19c0a5fd7c742465a1511bf3aee17925221bd157
Malthe <malthegram22@gmail.com>
2023-05-30 18:28:35 +0200

    update db conn string

--- database/database.js
+++ database/database.js
@@ -2,8 +2,9 @@ import {MongoClient, MongoError} from "mongodb"
 
 const PORT = process.env.MONGODB_PORT || 27017
 const CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING || "mongodb://127.0.0.1"
+const CONNECTION_STRING_CLOUD = process.env.MONGODB_CONNECTION_STRING_CLOUD
 
-const url = `${CONNECTION_STRING}:${PORT}`
+const url = CONNECTION_STRING_CLOUD //`${CONNECTION_STRING}:${PORT}`
 const client = new MongoClient(url)
 
 await client.connect()
@@ -15,8 +16,6 @@ const posts = await db.collection("posts")
 const forums = await db.collection("forums")
 const conversations = await db.collection("conversations")
 const collections = await db.listCollections().toArray()
-
-
 
 
 export default {
