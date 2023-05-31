a8b3de07e8acf351873f54769f8299922c5f87cc
Malthe <malthegram22@gmail.com>
2023-05-31 12:24:46 +0200

    update api to send json

--- routers/conversationRouter.js
+++ routers/conversationRouter.js
@@ -1,6 +1,6 @@
-import { Router } from "express"
+import {Router} from "express"
 import db from "../database/database.js"
-import { authenticateToken } from "./middelware/verifyJwt.js"
+import {authenticateToken} from "./middelware/verifyJwt.js"
 const router = Router();
 import {ObjectId} from "mongodb";
 
@@ -8,34 +8,42 @@ import {ObjectId} from "mongodb";
 router.get("/api/conversations/:conversationid", authenticateToken, async (req, res) => {
     const conversationId = req.params.conversationid;
     if (!ObjectId.isValid(conversationId)) {
-      return res.status(400).send("Invalid conversation id");
+        return res.status(400).send("Invalid conversation id");
     }
     try {
-      const findConversation = await db.conversations.findOne({_id: new ObjectId(conversationId)})
-      if (!findConversation) {
-        return res.status(404).send("Conversation not found");
-      }
-      res.send(findConversation);
+        const findConversation = await db.conversations.findOne({_id: new ObjectId(conversationId)})
+        if (!findConversation) {
+            return res.status(404).send("Conversation not found");
+        }
+        res.send(findConversation);
     } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal server error");
+        console.error(err);
+        res.status(500).send("Internal server error");
     }
-  })
+})
 
 //Find all conversations on a logged in user
 router.get("/api/conversations", authenticateToken, async (req, res) => {
-  const user = req.user.artistName;
-  const conversations = await db.conversations.find({ participants: { $in: [user] } }).project({ participants: { $elemMatch: { $ne: user } }, timeStamp: 1, read: 1, sender: 1 }).sort({ timeStamp: -1 }).toArray();
-  
-  const arrayParticipants = conversations.map(conversation => conversation.participants).flat();
-  const findUsers = await db.users.find({ artistName: { $in: arrayParticipants } }).project({ profilePictureKey: 1, artistName: 1}).toArray();
+    const user = req.user.artistName;
+    const conversations = await db.conversations.find({participants: {$in: [user]}}).project({
+        participants: {$elemMatch: {$ne: user}},
+        timeStamp: 1,
+        read: 1,
+        sender: 1
+    }).sort({timeStamp: -1}).toArray();
 
-  const mergedConversations = conversations.map((conversation) => {
-    const user = findUsers.find(u => u.artistName === conversation.participants[0]);
-    return { ...conversation, profilePictureKey: user?.profilePictureKey };
-  });
+    const arrayParticipants = conversations.map(conversation => conversation.participants).flat();
+    const findUsers = await db.users.find({artistName: {$in: arrayParticipants}}).project({
+        profilePictureKey: 1,
+        artistName: 1
+    }).toArray();
 
-  res.status(200).send(mergedConversations);
+    const mergedConversations = conversations.map((conversation) => {
+        const user = findUsers.find(u => u.artistName === conversation.participants[0]);
+        return {...conversation, profilePictureKey: user?.profilePictureKey};
+    });
+
+    res.status(200).send(mergedConversations);
 });
 
 //Creates a conversation with another user
@@ -45,20 +53,31 @@ router.post("/api/conversations/:receiver", authenticateToken, async (req, res) 
     const receiver = req.params.receiver;
     const timeStamp = new Date().toLocaleString("en-GB");
     const existingConversation = await db.conversations.findOne({participants: {$all: [sender, receiver]}});
-    if(existingConversation){
-      res.send({message: "Conversation already exist"})
-    }else{
-      const conversation = req.body;
-      conversation.participants = [sender, receiver];
-      conversation.messages = [{_id: new ObjectId(), timeStamp: timeStamp, body: `Hello this is a new conversation created at ${timeStamp}`}];
-      conversation.timeStamp = timeStamp;
-      conversation.read = false;
-      conversation.sender = sender;
-      const insertConversation = await db.conversations.insertOne(conversation);
-      const getImgReceiver = await db.users.findOne({artistName: receiver})
-      const getImgSender = await db.users.findOne({artistName: sender})
-      
-      res.status(200).send([{receiver: [receiver], sender: [sender], _id: insertConversation.insertedId, profilePictureKeyReceiver: getImgReceiver.profilePictureKey, profilePictureKeySender: getImgSender.profilePictureKey , timeStamp: conversation.timeStamp}]);
+    if (existingConversation) {
+        res.send({message: "Conversation already exist"})
+    } else {
+        const conversation = req.body;
+        conversation.participants = [sender, receiver];
+        conversation.messages = [{
+            _id: new ObjectId(),
+            timeStamp: timeStamp,
+            body: `Hello this is a new conversation created at ${timeStamp}`
+        }];
+        conversation.timeStamp = timeStamp;
+        conversation.read = false;
+        conversation.sender = sender;
+        const insertConversation = await db.conversations.insertOne(conversation);
+        const getImgReceiver = await db.users.findOne({artistName: receiver})
+        const getImgSender = await db.users.findOne({artistName: sender})
+
+        res.status(200).send([{
+            receiver: [receiver],
+            sender: [sender],
+            _id: insertConversation.insertedId,
+            profilePictureKeyReceiver: getImgReceiver.profilePictureKey,
+            profilePictureKeySender: getImgSender.profilePictureKey,
+            timeStamp: conversation.timeStamp
+        }]);
     }
 })
 
@@ -80,25 +99,25 @@ router.patch("/api/conversations/messages/:conversationid", authenticateToken, a
     message._id = new ObjectId();
     message.timeStamp = timeStamp
     const updateConversation = await db.conversations.updateOne(
-      { _id: new ObjectId(conversationId) },
-      { $set: { timeStamp: timeStamp, read: false, sender: user.artistName} }
+        {_id: new ObjectId(conversationId)},
+        {$set: {timeStamp: timeStamp, read: false, sender: user.artistName}}
     );
-    const insertMessage = await db.conversations.updateOne({ _id: new ObjectId(conversationId) }, { $push: { messages: message } });
-    res.status(200).send({ message: message });
+    const insertMessage = await db.conversations.updateOne({_id: new ObjectId(conversationId)}, {$push: {messages: message}});
+    res.status(200).send({message: message});
 })
 
 router.patch("/api/conversations/read/:coversationid", authenticateToken, async (req, res) => {
     const conversationId = new ObjectId(req.params.coversationid);
     const updateConversation = await db.conversations.updateOne(
-      { _id: conversationId},
-      { $set: { read: true } }
+        {_id: conversationId},
+        {$set: {read: true}}
     );
     res.status(200).send(updateConversation)
 })
 
 router.delete("/api/conversations/:conversationid", async (req, res) => {
     const conversationId = new ObjectId(req.params.conversationid);
-    const deleteConversation = await db.conversations.deleteOne({_id : conversationId})
+    const deleteConversation = await db.conversations.deleteOne({_id: conversationId})
     res.status(200).send(deleteConversation)
 })
 
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -30,108 +30,111 @@ let tags = [
 //get all posts
 router.get("/api/posts", async (req, res) => {
     try {
-      const posts = await db.posts.find().toArray();
-      res.send(posts);
+        const posts = await db.posts.find().toArray();
+        res.send({posts});
     } catch (error) {
-      res.status(500).send('Internal Server Error');
+        res.status(500).send({message: 'Internal Server Error'});
     }
-  });
+});
 
 
 //News page
 
 router.get("/api/posts/news", authenticateToken, async (req, res) => {
     const userLoggedIn = req.user;
-    const dbUser = await db.users.findOne({ _id: new ObjectId(userLoggedIn._id) });
-  
+    const dbUser = await db.users.findOne({_id: new ObjectId(userLoggedIn._id)});
+
     if (dbUser !== null) {
-      const posts = db.posts.find({ artistName: { $in: dbUser?.following } }).sort({ "timeStamp": -1 });
-      const postArray = await posts.toArray();
-      let filteredArray = postArray.filter(post => post.referenceName === "wallpost");
-  
-      const parseDateString = (dateString) => {
-        const [datePart, timePart] = dateString.split(", ");
-        const [day, month, year] = datePart.split("/");
-        const [hours, minutes, seconds] = timePart.split(":");
-  
-        // Note: The month value is zero-based in JavaScript's Date object,
-        // so we subtract 1 from the parsed month value
-        return new Date(year, month - 1, day, hours, minutes, seconds); 
-      };
-  
-      filteredArray.forEach((post) => {
-        const sortedComments = [...post.comments];
-        sortedComments.sort((a, b) => {
-          const dateA = parseDateString(a.timeStamp);
-          const dateB = parseDateString(b.timeStamp);
-          return dateB - dateA;
+        const posts = db.posts.find({artistName: {$in: dbUser?.following}}).sort({"timeStamp": -1});
+        const postArray = await posts.toArray();
+        let filteredArray = postArray.filter(post => post.referenceName === "wallpost");
+
+        const parseDateString = (dateString) => {
+            const [datePart, timePart] = dateString.split(", ");
+            const [day, month, year] = datePart.split("/");
+            const [hours, minutes, seconds] = timePart.split(":");
+
+            // Note: The month value is zero-based in JavaScript's Date object,
+            // so we subtract 1 from the parsed month value
+            return new Date(year, month - 1, day, hours, minutes, seconds);
+        };
+
+        filteredArray.forEach((post) => {
+            const sortedComments = [...post.comments];
+            sortedComments.sort((a, b) => {
+                const dateA = parseDateString(a.timeStamp);
+                const dateB = parseDateString(b.timeStamp);
+                return dateB - dateA;
+            });
+            post.comments = sortedComments;
         });
-        post.comments = sortedComments;
-      });
-  
-      res.status(200).send(filteredArray);
+
+        res.status(200).send(filteredArray);
     }
-  
+
     if (dbUser === null) {
-      res.status(200).send({ message: "no posts" });
+        res.status(200).send({message: "no posts"});
     } else {
-      res.status(500).send();
+        res.status(500).send();
     }
-  });
+});
 
 //Posts on own profile
 router.get("/api/posts/wallposts/:artistName", async (req, res) => {
     try {
-      const artistName = req.params.artistName;
-  
-      const wallPosts = await db.posts.find({ referenceName: "wallpost", artistName: artistName }).sort({ "timeStamp": -1 });
-      const postArray = await wallPosts.toArray();
-  
-      const parseDateString = (dateString) => {
-        const [datePart, timePart] = dateString.split(", ");
-        const [day, month, year] = datePart.split("/");
-        const [hours, minutes, seconds] = timePart.split(":");
-  
-        // Note: The month value is zero-based in JavaScript's Date object,
-        // so we subtract 1 from the parsed month value
-        return new Date(year, month - 1, day, hours, minutes, seconds);
-      };
-  
-      postArray.forEach((post) => {
-        const sortedComments = [...post.comments];
-        sortedComments.sort((a, b) => {
-          const dateA = parseDateString(a.timeStamp);
-          const dateB = parseDateString(b.timeStamp);
-          return dateB - dateA;
+        const artistName = req.params.artistName;
+
+        const wallPosts = await db.posts.find({
+            referenceName: "wallpost",
+            artistName: artistName
+        }).sort({"timeStamp": -1});
+        const postArray = await wallPosts.toArray();
+
+        const parseDateString = (dateString) => {
+            const [datePart, timePart] = dateString.split(", ");
+            const [day, month, year] = datePart.split("/");
+            const [hours, minutes, seconds] = timePart.split(":");
+
+            // Note: The month value is zero-based in JavaScript's Date object,
+            // so we subtract 1 from the parsed month value
+            return new Date(year, month - 1, day, hours, minutes, seconds);
+        };
+
+        postArray.forEach((post) => {
+            const sortedComments = [...post.comments];
+            sortedComments.sort((a, b) => {
+                const dateA = parseDateString(a.timeStamp);
+                const dateB = parseDateString(b.timeStamp);
+                return dateB - dateA;
+            });
+            post.comments = sortedComments;
         });
-        post.comments = sortedComments;
-      });
-  
-      res.status(200).send(postArray);
-    } catch (error) {
-      res.status(500).send('Internal Server Error');
-    }
-  });
 
-  router.get("/api/posts/tags", (req, res) => {
-    try {
-      res.status(200).send({ tags });
+        res.status(200).send(postArray);
     } catch (error) {
-      res.status(500).send('Internal Server Error');
+        res.status(500).send('Internal Server Error');
     }
-  });
+});
 
-  router.get("/api/posts/hyped", async (req, res) => {
+router.get("/api/posts/tags", (req, res) => {
     try {
-      const postEligibleForHype = await db.posts.find({
-        $expr: { $gte: [{ $size: "$rating" }, 2] }
-      }).toArray();
-  
-      res.status(200).send(postEligibleForHype);
+        res.status(200).send({tags});
     } catch (error) {
-      res.status(500).send('Internal Server Error');
+        res.status(500).send('Internal Server Error');
     }
-  });
+});
+
+router.get("/api/posts/hyped", async (req, res) => {
+    try {
+        const postEligibleForHype = await db.posts.find({
+            $expr: {$gte: [{$size: "$rating"}, 2]}
+        }).toArray();
+
+        res.status(200).send(postEligibleForHype);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 
 //Create a post from the user and reference is where the post is located
@@ -212,37 +215,37 @@ router.patch("/api/posts/comments/:commentId", authenticateToken, async (req, re
 
 router.patch("/api/posts/comments/:reference/:search", authenticateToken, async (req, res) => {
     try {
-      const user = req.user;
-      const comment = req.body;
-      const findUser = await db.users.findOne({ artistName: user.artistName });
-      comment.commentAuthor = user.artistName;
-      comment._id = new ObjectId();
-      comment.rating = [];
-      comment.reported = [];
-      comment.timeStamp = new Date().toLocaleString("en-GB");
-      comment.artistId = user._id;
-      comment.profilePictureKey = findUser.profilePictureKey;
-  
-      if (req.params.reference === "wallposts") {
-        const id = req.params.search;
-        const result = await db.posts.updateOne(
-          { _id: new ObjectId(id) },
-          { $push: { comments: comment } }
-        );
-        res.status(200).send({ message: comment });
-      } else {
-        const postId = new ObjectId(req.params.search);
-        const updateCommentArray = await db.posts.updateOne(
-          { _id: postId },
-          { $push: { comments: comment } }
-        );
-        res.status(200).send(comment);
-      }
+        const user = req.user;
+        const comment = req.body;
+        const findUser = await db.users.findOne({artistName: user.artistName});
+        comment.commentAuthor = user.artistName;
+        comment._id = new ObjectId();
+        comment.rating = [];
+        comment.reported = [];
+        comment.timeStamp = new Date().toLocaleString("en-GB");
+        comment.artistId = user._id;
+        comment.profilePictureKey = findUser.profilePictureKey;
+
+        if (req.params.reference === "wallposts") {
+            const id = req.params.search;
+            const result = await db.posts.updateOne(
+                {_id: new ObjectId(id)},
+                {$push: {comments: comment}}
+            );
+            res.status(200).send({message: comment});
+        } else {
+            const postId = new ObjectId(req.params.search);
+            const updateCommentArray = await db.posts.updateOne(
+                {_id: postId},
+                {$push: {comments: comment}}
+            );
+            res.status(200).send(comment);
+        }
     } catch (error) {
-      console.error('Error patching comments:', error);
-      res.status(500).send('Internal Server Error');
+        console.error('Error patching comments:', error);
+        res.status(500).send('Internal Server Error');
     }
-  });
+});
 
 router.patch("/api/posts/:postid", authenticateToken, async (req, res) => {
     const postId = new ObjectId(req.params.postid)
