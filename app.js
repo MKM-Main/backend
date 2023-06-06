5af1860d6a8fe18680f8d84aa671ab4058304fd9
Malthe <malthegram22@gmail.com>
2023-06-06 11:33:26 +0200

    update env

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: [process.env.SITE_URL],
         credentials: true
     }
 })
