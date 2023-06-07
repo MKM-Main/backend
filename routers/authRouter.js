f09a9b553c689849bb083a40672551cc6cc1861f
Malthe <malthegram22@gmail.com>
2023-06-07 13:13:50 +0200

    update cookie optios

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -63,9 +63,7 @@ router.post("/api/auth/login", async (req, res) => {
         if (await bcrypt.compare(user.password, hashedPassword)) {
             delete findUserByEmail[0].password
             const accessToken = jwt.sign(findUserByEmail[0], jwtSecret, {expiresIn: "120m"});
-            res.set('Access-Control-Allow-Origin', req.headers.origin)
-            res.set('Access-Control-Allow-Credentials', 'true')
-            res.cookie('jwt', accessToken, {httpOnly: true, secure: true, sameSite: "strict"});
+            res.cookie('jwt', accessToken, {httpOnly: false, secure: false, sameSite: "lax"});
             res.status(200).send({data: "Success"})
         }
     } catch (error) {
