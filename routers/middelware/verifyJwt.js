35496dadf5170e60af143bde23ddbcf58596a9f9
Kevin <kevi4777@stud.kea.dk>
2023-06-14 17:17:30 +0200

    index-fix

--- routers/middelware/verifyJwt.js
+++ routers/middelware/verifyJwt.js
@@ -10,7 +10,7 @@ export function authenticateToken(req, res, next) {
     return res.status(401).json({message: 'Unauthorized 1'});
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, jwtSecret, (err, user) => {
     if (err) {
       // Check if the error is due to token expiration
       if (err.name === 'TokenExpiredError') {
@@ -28,40 +28,3 @@ export function authenticateToken(req, res, next) {
     next();
   });
 }
-
-// export function authenticateToken (req, res, next) {
-//     const authHeader = req.headers['authorization']
-//     const token = authHeader && authHeader.split(' ')[1]
-//     if (!token) return res.sendStatus(401)
-//     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//       if (err) return res.sendStatus(403)
-//       req.user = user
-//       next()
-//     })
-// }
-
-
-// export const authMiddleware = (req, res, next) => {
-//     // Get the JWT token from the Authorization header
-//     const authHeader = req.headers.authorization;
-//     const token = authHeader && authHeader.split(' ')[1];
-
-//     if (!token) {
-//       return res.status(401).json({ message: 'Auth failed!' });
-//     }
-
-//     try {
-//       // Verify the JWT token using the secret key
-//       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-//       // Extract the user ID and email from the token's payload
-//       const userId = decodedToken.id;
-//       const userEmail = decodedToken.email;
-//       // Store the user information in the request object
-//       req.userId = userId;
-//       req.userEmail = userEmail;
-//       // Call the next middleware function
-//       next();
-//     } catch (error) {
-//       res.status(401).json({ message: 'Auth failed!' });
-//     }
-//   };
